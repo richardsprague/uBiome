be9b5581fd817ee5675b9a60413b365949762ee6
Richard Sprague <richardsprague@gmail.com>
2015-01-18 19:51:00 -0800

    new screenshot for Json dump

---
+++ uBiome tracking.R
@@ -0,0 +1,6 @@
+may<-read.csv("sprague-uBiomeJun2014.csv")
+may$ubiome_bacteriacounts.tax_name
+may$ubiome_bacteriacounts.tax_rank=="species"
+may_species <- may[may$ubiome_bacteriacounts.tax_rank=="species",]
+head(may$ubiome_bacteriacounts.tax_rank)
+summary(may[may$ubiome_bacteriacounts.tax_rank,])
